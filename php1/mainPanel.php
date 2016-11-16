5eef89af305e24fe664ce22a18bb5dbbb6ca4a83
AtomowaAneta <marstyczen@gmail.com>
2016-11-16 17:17:58 +0100

    app can be dropped on different wrappers

--- php1/mainPanel.php
+++ php1/mainPanel.php
@@ -182,9 +182,23 @@
 			}, "jsonp");
 
       $(function() {
-        $(".draggable").draggable();
+        $(".draggable").draggable({
+          appendTo: 'app_wrapper',
+          revert: "invalid",
+          start: function(e,ui){ui.helper.width($(this).width());}
+        });
         $(".droppable").droppable({
           hoverClass: "hover_on_app_wrapper",
+          drop: function(e,ui) {
+            var item = ui.draggable;
+            this.innerHTML = '';
+            item.css({top:0, left:0}).appendTo(this);
+          },
+          out: function(e,ui) {
+
+            $(this).droppable("enable");
+          }
+
       });
       });
 
