be3543bae98196a0f23b33c4fe64f011f79c611d
AtomowaAneta <marstyczen@gmail.com>
2016-11-16 16:34:50 +0100

    jqueryui added, working on dropping

--- js/hourApp.js
+++ js/hourApp.js
@@ -9,14 +9,14 @@ myApp.controller('myCtrl', function($scope, $interval) {
 
 myApp.controller('toDoAppController', function($scope) {
 	$scope.toDotitle = "What to do?";
-	$scope.tasks = ["Cut the grass", "fuck your wife", "awd", "a" ,"sefesf"];
+	$scope.tasks = ["Cut the grass", "fuck your wife",];
 		$scope.addTask = function (){
-			$scope.errortext = "";
+			$scope.error = "";
        	 if (!$scope.addGiven) {return;}
        		 if ($scope.tasks.indexOf($scope.addGiven) == -1) {
             		$scope.tasks.push($scope.addGiven);
         } else {
-            $scope.errortext = "The task is on the list!";
+            $scope.error = "The task is on the list!";
         }
 
 		};
@@ -27,12 +27,12 @@ myApp.controller('toDoAppController', function($scope) {
 });
 
 
-myApp.controller('monthDisplayer' function($scope) {
+myApp.controller('monthDisplayer' ,function($scope) {
 	$scope.actualMonth = '';
 	$scope.months = [ 'January', 'February', 'March', 'April', 'May', 'June',
             'July', 'August', 'September', 'October', 'November', 'December' ];
     $scope.printMonth = function () {
-    	var monthNumber = new Date().getMonth();
+    	 var monthNumber = new Date().getMonth();
     	$scope.actualMonth = months[monthNumber];
     }
 
--- php1/mainPanel.php
+++ php1/mainPanel.php
@@ -24,16 +24,15 @@
       <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Fjalla+One" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet">
       <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
       <script src="/php_proj/js/hourApp.js"></script>
       <script src="/php_proj/js/calendar.js"></script>
       <link rel="icon" type="image/png" href="/php_proj/img/question.jpg">
-
-     
    </head>
    <style> 
-   .main_wrapper {
-   	padding-top: 70px;
-   }
+      .main_wrapper {
+      padding-top: 70px;
+      }
    </style>
    <body ng-app="myApp">
       <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -59,110 +58,119 @@
       <div class="row main_wrapper">
          <div class="col-md-3 left_main">
             <div class="row">
-         		<div class="location_info" data-toggle="tooltip" title="aa here you are!">
-         			<div class="well">
-         			<div class="location_head" >Location info</div>
-	         		<div id="ip"></div>
-	         		<div id="city_name"></div>
-	         		<div id="province_name"></div>
-	         		<div id="country_name"></div>
-	         		<div id="coordinates"></div>
-	         		<div style="margin-top: 10px; margin-left: 55px;" class="location_head btn" >See on map</div>
-	         		</div>
-         	</div>
-            </div>
-            </br> </br> </br> </br> </br> </br> 
-            <div class="row">
-       
-               <div class="calendar_bg">
-                    <div class="panel panel-default">
-                    	<div class="panel-heading" style="text-align:center; font-family : 'Shrikhand', cursive">Calendar</div>
-                    	<div class="panel-body">
-                    		<p id="month">
-                    			
-                    		</p>
-                    	</div>
-                    	<div class="panel-footer"></div>
-                  
+               <div class="app_wrapper droppable ">
+                  <div class="well draggable">
+                     <div class="location_head" >Location info</div>
+                     <div id="ip"></div>
+                     <div id="city_name"></div>
+                     <div id="province_name"></div>
+                     <div id="country_name"></div>
+                     <div id="coordinates"></div>
+                     <div style="margin-top: 10px; margin-left: 55px;" class="location_head btn" >See on map</div>
+                  </div>
                </div>
+            </div>
+            </br>  
+            <div class="row">
+              
+
+               <div class="app_wrapper droppable" >
+                  <div class="well   draggable" >
+                    
+                     
+                        <p id="month">a                        </p>
+                                   
+                  </div>
+               </div>
+            </div>
+                </br> 
+             <div class="row">
+              
+
+               <div class="app_wrapper droppable" >
+                  <div class="well  draggable" >
+                    
+                     
+      
+                                   
+                  </div>
                </div>
             </div>
          </div>
-         <div class="col-md-6 post_bg"> Tu posty bedo
-         <h1 style="color: red;">{{errortext}}</h1> </div>
+         <div class="col-md-6 post_bg draggable">
+            Tu posty bedo
+            <h1 style="color: red;">{{error}}</h1>
+         </div>
          <div class="col-md-3 right_main"  style="color:black;">
             <div class="row">
-               <div class="todo_bg">
-   			<div  ng-controller="toDoAppController">
-   			<div class="panel-group">
-   				<div class="panel panel-default">
-   				<div class="panel-heading toDo_title" style="text-align: center; color:" data-toggle="tooltip" title="Plan your tasks">{{toDotitle}}</div>
-				  
-				    <div style="text-align:center;" id="panel_body" class="panel-body " ng-repeat="x in tasks">{{x}}
-						<span class="glyphicon glyphicon-remove" ng-click="deleteTask()"> </span>
-				    </div>
-				    
-				<div class="panel-footer">  
-				  <input  ng-model="addGiven" id="toDo_input">
-				  <button class="btn btn-success" ng-click="addTask()">Add</button>
-
-				</div>
-				</div>
-				</div>
-   				</div>
-   			</div>
+               <div class="app_wrapper droppable">
+                  <div  ng-controller="toDoAppController">
+                     <div class="panel-group">
+                        <div class="panel panel-default draggable">
+                           <div class="panel-heading toDo_title" style="text-align: center; color:" data-toggle="tooltip" title="Plan your tasks">{{toDotitle}}</div>
+                           <div style="text-align:center;" id="panel_body" class="panel-body " ng-repeat="x in tasks">{{x}}
+                              <span class="glyphicon glyphicon-remove" ng-click="deleteTask()"> </span>
+                           </div>
+                           <div class="panel-footer">  
+                              <input  ng-model="addGiven" id="toDo_input">
+                              <button class="btn btn-success" ng-click="addTask()">Add</button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
                </div>
-                </br> 
+            </div>
+            </br> 
             <div class="row">
-               <div class="news_bg">
-                 
-               </div>
+               <div class="app_wrapper droppable ">
+              </div>
             </div>
+            </br>
+             <div class="row">
+               <div class="app_wrapper droppable ">
+              </div>
             </div>
-           
          </div>
       </div>
       </div>
       </div>
       </div>
-      <footer class="footer navbar-fixed-bottom"> 
-      <div class="row-fluid">
-      <div class="col-md-4" style="padding-top: 3px; font-family: 'Shrikhand', cursive ">
-     
- <div class="dropup">
-    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" >Begin
-      <span class="caret"></span> </button>
-    <ul class="dropdown-menu">
-	<li style="text-align:center;"><a href="#"><?php echo $nick?> <span class="glyphicon glyphicon-user"></span></a></li>
-		<li class="divider"></li>
-      
-      <li class="dropdown-toggle"><a href="#">Applications   <span class="glyphicon glyphicon-arrow-right"></span></a>
-
-      <ul class="dropdown-menu">
-      <li><a href="#">Customize</a></li>
-       <li><a href="#">Customize</a></li>
-      </ul>
-
-      </li>
-      <li><a href="#">Customize</a></li>
-      
-      <li class="divider"></li>
-      <li style="text-align:center;"><a href="logOut.php">Log out <span class="glyphicon glyphicon-off"></span></a></li>
-    </ul>
-  </div>
-
       </div>
-           <div class="col-md-8">
-      <div id="time"  ng-controller="myCtrl">
-               <p>{{theTime}}</p>
+      <footer class="footer navbar-fixed-bottom">
+         <div class="row-fluid">
+            <div class="col-md-4" style="padding-top: 3px; font-family: 'Shrikhand', cursive ">
+               <div class="dropup">
+                  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" >Begin
+                  <span class="caret"></span> </button>
+                  <ul class="dropdown-menu">
+                     <li style="text-align:center;"><a href="#"><?php echo $nick?> <span class="glyphicon glyphicon-user"></span></a></li>
+                     <li class="divider"></li>
+                     <li class="dropdown-toggle">
+                        <a href="#">Applications   <span class="glyphicon glyphicon-arrow-right"></span></a>
+                        <ul class="dropdown-menu">
+                           <li><a href="#">Customize</a></li>
+                           <li><a href="#">Customize</a></li>
+                        </ul>
+                     </li>
+                     <li><a href="#">Customize</a></li>
+                     <li class="divider"></li>
+                     <li style="text-align:center;"><a href="logOut.php">Log out <span class="glyphicon glyphicon-off"></span></a></li>
+                  </ul>
+               </div>
             </div>
+            <div class="col-md-8">
+               <div id="time"  ng-controller="myCtrl">
+                  <p>{{theTime}}</p>
+               </div>
             </div>
-            </div>
-       </footer>
+         </div>
+      </footer>
        <script>
-			$(document).ready(function(){
-			    $('[data-toggle="tooltip"]').tooltip();
-			});
+			$( function() {
+       $( document ).tooltip();
+     } );
+    
+ 
 
 			$.get("http://ipinfo.io", function (response) {
 			    $("#ip").html("IP: " + response.ip);
@@ -172,6 +180,14 @@
 			    $("#coordinates").html("Coordinates: " + response.loc);
 			   
 			}, "jsonp");
+
+      $(function() {
+        $(".draggable").draggable();
+        $(".droppable").droppable({
+          hoverClass: "hover_on_app_wrapper",
+      });
+      });
+
 
 
 </script>
